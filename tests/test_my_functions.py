bca1f163934ff6267cff8cb5a913ce7519d00e0d
504BassSlapper <504bassslapper@gmail.com>
2023-10-25 09:41:37 +0100

    add unit test for zerodivision errot

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,6 +1,18 @@
 import pytest
 import source.my_functions as my_functions
 
+
 def test_add():
     result = my_functions.add_two_numbers(n1=3, n2=4)
     assert result == 7
+
+
+def test_division():
+    result = my_functions.divide_two_numbers(n1=30, n2=5)
+    assert result == 6
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide_two_numbers(n1=90, n2=0)
+        assert True
